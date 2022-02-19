f24eddb33034628e9a71cf19e997de6afaf4d39d
Zensonaton <zensonaton@gmail.com>
2022-02-20 00:44:58 +0600

    Fix matching regex.

--- src-tm/main.js
+++ src-tm/main.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Bilimland autosolver
 // @namespace    https://github.com/Zensonaton/BL-Autosolver
-// @version      1.1.2
+// @version      1.1.3
 // @updateURL    https://github.com/Zensonaton/BL-Autosolver/releases/latest/download/Bilimland-Autosolver-Release.js
 // @downloadURL  https://github.com/Zensonaton/BL-Autosolver/releases/latest/download/Bilimland-Autosolver-Release.js
 // @description  Скрипт для автоматического выставления ответов в Bilimland'е.
 // @author       Zensonaton
-// @match        /^https:\/\/onlinemektep\.(?:org|net)\/schedule\/*$/
+// @match        ^https:\/\/onlinemektep\.(?:org|net)\/schedule\/.*\/lesson\/.*$
 // @icon         https://www.google.com/s2/favicons?domain=onlinemektep.net
 // @grant        none
 // ==/UserScript==
