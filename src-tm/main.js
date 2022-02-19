05d414a044bda6522f9ea7b803a63afa5a87212c
Zensonaton <zensonaton@gmail.com>
2022-02-20 00:57:34 +0600

    Fix URL, add license

--- src-tm/main.js
+++ src-tm/main.js
@@ -1,14 +1,16 @@
 // ==UserScript==
 // @name         Bilimland autosolver
 // @namespace    https://github.com/Zensonaton/BL-Autosolver
-// @version      1.1.3
+// @version      1.1.1
 // @updateURL    https://github.com/Zensonaton/BL-Autosolver/releases/latest/download/Bilimland-Autosolver-Release.js
 // @downloadURL  https://github.com/Zensonaton/BL-Autosolver/releases/latest/download/Bilimland-Autosolver-Release.js
 // @description  Скрипт для автоматического выставления ответов в Bilimland'е.
 // @author       Zensonaton
-// @match        ^https:\/\/onlinemektep\.(?:org|net)\/schedule\/.*\/lesson\/.*$
+// @include      *://onlinemektep.net/schedule/*/lesson/*
+// @include      *://onlinemektep.org/schedule/*/lesson/*
 // @icon         https://www.google.com/s2/favicons?domain=onlinemektep.net
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 // Made by Zensonaton with love.
