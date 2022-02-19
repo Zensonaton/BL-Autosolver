deb6ce8eb9e93517227f146d18df06aef3472846
Zensonaton <zensonaton@gmail.com>
2022-02-20 00:36:14 +0600

    Version.

--- src-tm/main.js
+++ src-tm/main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bilimland autosolver
 // @namespace    https://github.com/Zensonaton/BL-Autosolver
-// @version      1.1
+// @version      1.1.1
 // @updateURL    https://github.com/Zensonaton/BL-Autosolver/releases/latest/download/main.js
 // @downloadURL  https://github.com/Zensonaton/BL-Autosolver/releases/latest/download/main.js
 // @description  Скрипт для автоматического выставления ответов в Bilimland'е.
