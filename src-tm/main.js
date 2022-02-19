27f46f3b36a656cc60092a99b413884546a7ce9e
Zensonaton <zensonaton@gmail.com>
2022-02-20 00:40:50 +0600

    Fix update.

--- src-tm/main.js
+++ src-tm/main.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Bilimland autosolver
 // @namespace    https://github.com/Zensonaton/BL-Autosolver
-// @version      1.1.1
-// @updateURL    https://github.com/Zensonaton/BL-Autosolver/releases/latest/download/main.js
-// @downloadURL  https://github.com/Zensonaton/BL-Autosolver/releases/latest/download/main.js
+// @version      1.1.2
+// @updateURL    https://github.com/Zensonaton/BL-Autosolver/releases/latest/download/Bilimland-Autosolver-Release.js
+// @downloadURL  https://github.com/Zensonaton/BL-Autosolver/releases/latest/download/Bilimland-Autosolver-Release.js
 // @description  Скрипт для автоматического выставления ответов в Bilimland'е.
 // @author       Zensonaton
 // @match        /^https:\/\/onlinemektep\.(?:org|net)\/schedule\/*$/
